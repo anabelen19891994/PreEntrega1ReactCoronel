4acad4ee52171ebb779cc4bf2c5f8fe7964cacc8
anabelen19891994 <ana.b.coronel@outlook.com>
2024-02-20 19:28:21 -0300

    instalación de firebase

--- src/main.jsx
+++ src/main.jsx
@@ -2,4 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCR49VBBdgSOflmMFAvwUa9pjhkNTM7UDo",
+  authDomain: "pelitos-petshop.firebaseapp.com",
+  projectId: "pelitos-petshop",
+  storageBucket: "pelitos-petshop.appspot.com",
+  messagingSenderId: "1070269785584",
+  appId: "1:1070269785584:web:0e473541a6caadc33f2935"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
