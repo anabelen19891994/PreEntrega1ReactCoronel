0c4e1941d29393b7478ff3cdb5df2ec2207ff6e0
anabelen19891994 <ana.b.coronel@outlook.com>
2024-02-23 12:08:21 -0300

    formulario

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,23 +1,42 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { CartContext } from "./context/CartContext";
 import { Link } from "react-router-dom";
+import { getFirestore, collection, addDoc } from "firebase/firestore";
 
 const Checkout = () => {
+    const [nombre, setNombre] = useState();
+    const [email, setEmail] = useState();
+    const [telefono, setTelefono] = useState();
+    const [orderId, setOrderId] = useState();
+    const {cart, clear, CantTotalProductos, SumaTotalProductos} = useContext(CartContext);
 
-        const {cart, CantTotalProductos, SumaTotalProductos} = useContext(CartContext);
+        const generarOrden = () => {
+            if (nombre.length === 0) {
+                return false;
+            }
 
-        if (CantTotalProductos() == 0) {
-            return (
-                <div className="container">
-                    <div className="row">
-                        <div className="col text-center">
-                            <div className="alert alert-danger" role="alert">No se encontraron productos en el carrito</div>
-                            <Link to={"/"} className="btn btn-success my-5">Volver a la página principal</Link>
-    
-                        </div>
-                    </div>
-                </div>
-            )
+            if (email.length === 0) {
+                return false;
+            }
+
+            if (telefono.length === 0) {
+                return false;
+            }
+
+            const buyer = {name:nombre, email:email, phone:telefono};
+            const items = cart.map(item => ({id:item.idx, title:item.title, price:item.price}));
+            const fecha = new Date();
+            const date = `${fecha.getDate()}-${fecha.getMonth()+1}-${fecha.getFullYear()} ${fecha.getHours()}:${fecha.getMinutes()}`;
+            const total = SumaTotalProductos();
+            const order = {buyer:buyer, items:items, date:date, total:total};
+
+            const db = getFirestore();
+            const ordersCollection = collection(db, "orders");
+            addDoc(ordersCollection, order).then(resultado => {
+                clear();
+                setOrderId(resultado.id);
+            });
+            
         }
         
         return (
@@ -33,20 +52,20 @@ const Checkout = () => {
                         <form>
   <div className="mb-3">
     <label className="form-label">Nombre</label>
-    <input type="text" className="form-control"/>
+    <input type="text" className="form-control" onInput={(e) => {setNombre(e.target.value)}} />
   </div>
 
   <div className="mb-3">
     <label className="form-label">Email</label>
-    <input type="text" className="form-control"/>
+    <input type="text" className="form-control" onInput={(e) => {setEmail(e.target.value)}} />
   </div>
 
   <div className="mb-3">
     <label className="form-label">Teléfono</label>
-    <input type="text" className="form-control"/>
+    <input type="text" className="form-control" onInput={(e) => {setTelefono(e.target.value)}} />
   </div>
   
-  <button type="button" className="btn btn-success">Generar orden</button>
+  <button type="button" className="btn btn-success" onClick={generarOrden}>Generar orden</button>
 </form>
                         </div>
                         <div className="col-md-6 text-center">
@@ -68,6 +87,14 @@ const Checkout = () => {
                                 </tr>
                             </tbody>
                         </table>
+                    </div>
+                </div>
+                <div className="row my-5">
+                    <div className="col text-center">
+                        {orderId ? <div className="alert alert-success p-5 text-center" role="alert">
+                            <h1>Gracias por tu compra!</h1>
+                            <p>Tu ID de compra es: <b>{orderId}</b></p>
+                    </div> : ""}
                     </div>
                 </div>
             </div>
